4b929101aaf49c9652785c629f80618d5adb8519
namazii <nazar383875@gmail.com>
2022-06-16 09:27:20 +0300

    build(common):  initialized prject

---
+++ GitVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitVK
+//
+//  Created by Назар Ткаченко on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
