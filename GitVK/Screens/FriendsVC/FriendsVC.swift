d6e63cea9e31043b62ee78610bb9a018d0304341
namazii <nazar383875@gmail.com>
2022-07-25 16:53:53 +0300

    feat(SreensVC) add searchBar

--- GitVK/Screens/FriendsVC/FriendsVC.swift
+++ GitVK/Screens/FriendsVC/FriendsVC.swift
@@ -10,12 +10,13 @@ import UIKit
 final class FriendsVC: UIViewController {
     
     var rootView: FriendsView { return self.view as! FriendsView }
-    
     var friendsAPI = FriendsAPI()
-    
     var friends: [Friend] = []
+    var searchFriends: [Friend] = []
     
     var isFriendsLoading = false
+    var searchMode = false
+    
 
     //MARK: - Lifecycle
     override func loadView() {
@@ -26,18 +27,40 @@ final class FriendsVC: UIViewController {
         super.viewDidLoad()
         setupDelegates()
         fetchFriends(offset: 0)
+        navigationItem.titleView = rootView.searchBar
     }
     
-    //MARK: - Private Methods
+    
+    //MARK: - Private
+    private func setupDelegates() {
+        rootView.tableView.allowsSelection = false
+        rootView.tableView.delegate = self
+        rootView.tableView.dataSource = self
+        rootView.tableView.prefetchDataSource = self
+        rootView.searchBar.delegate = self
+    }
+
+    //MARK: - Requests
+    private func searchFriends(offset: Int = 0, search: String) {
+        Task {
+            do {
+                let friends = try await friendsAPI.searchFriends(offset: offset, search: search)
+                searchFriends = friends
+                rootView.tableView.reloadData()
+                
+            } catch {
+                print(error) //Alert -> error
+            }
+        }
+    }
     
     private func fetchFriends(offset: Int = 0) {
         
         //Старая асинхронность через колбэки/кложуры
         //Функция + колбэк (completion) -> асинхронный код -> на главном потоке
-        
         //Новая асинхронность (настоящая асинхронность) ->
-        
         //Task может запускать асинхронный код
+        
         Task {
             do {
                 let friends = try await friendsAPI.fetchFriends(offset: friends.count)
@@ -59,57 +82,59 @@ final class FriendsVC: UIViewController {
             }
         }
     }
-    
-    private func setupDelegates() {
-        
-        rootView.tableView.allowsSelection = false
-        rootView.tableView.delegate = self
-        rootView.tableView.dataSource = self
-        rootView.tableView.prefetchDataSource = self
-    }
 }
 
-//MARK: - Extension
+//MARK: - UITableViewDataSourcePrefetching
 extension FriendsVC: UITableViewDataSourcePrefetching {
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        
+
 //        print(indexPaths)
-        
         let maxRow = indexPaths.map { $0.last ?? 0 }.max() ?? 0
-        
 //        print(maxRow)
-        
         if maxRow > friends.count - 5, isFriendsLoading == false {
-            
             isFriendsLoading = true
-            
             fetchFriends(offset: friends.count)
         }
     }
 }
 
+
+//MARK: - UITableViewDataSource
 extension FriendsVC: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
+        return searchMode ? searchFriends.count : friends.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: FriendCell.identifier, for: indexPath) as? FriendCell else { return UITableViewCell() }
-        
-        let friend = self.friends[indexPath.row]
-        
+        let friend = searchMode == true ? self.searchFriends[indexPath.row] : self.friends[indexPath.row]
         cell.configure(friend)
-//        cell.textLabel?.text = friend.name
         
         return cell
     }
-    
-    
 }
 
 extension FriendsVC: UITableViewDelegate {
     
 }
 
+//MARK: - UISearchBarDelegate
+extension FriendsVC: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchMode = true
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchMode = false
+        rootView.tableView.reloadData()
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchFriends(offset: 0, search: searchText)
+    }
+}
